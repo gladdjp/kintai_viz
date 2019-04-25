9be0f1e6c7e012ae0368c5a5a5034b90851dac2a
Warut Surapat <wsurapat@gilt.jp>
2019-04-25 10:57:23 +0900

    Add status endpoint for hook test.

--- lib/kintai_viz_web/controllers/page_controller.ex
+++ lib/kintai_viz_web/controllers/page_controller.ex
@@ -4,4 +4,9 @@ defmodule KintaiVizWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def status(conn, params) do
+    # render(conn, "index.html")
+    text(conn, "#{params["challenge"]}")
+  end
 end
