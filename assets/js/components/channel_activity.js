24d40c0e5050020ff8185fe6030b234cbd4f4f80
Warut Surapat <wsurapat@gilt.jp>
2019-04-29 23:49:02 +0900

    Add placeholder for frontend components.

---
+++ assets/js/components/channel_activity.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ChannelActivity extends Component {
+  render() {
+    return (
+      <div>Channel Activity</div>
+    );
+  }
+}
+
+export default ChannelActivity;
---
+++ assets/js/components/member_activity.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MemberActivity extends Component {
+  render() {
+    return (
+      <div>Member Activity</div>
+    );
+  }
+}
+
+export default MemberActivity;
