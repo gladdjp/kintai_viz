1bde7e5c7528054874cb1c29ec910ae605cb5866
Warut Surapat <s.warut@gmail.com>
2019-05-01 17:53:28 +0900

    Add handler when user was already existed.

--- lib/kintai_viz/slack/slack_webhook.ex
+++ lib/kintai_viz/slack/slack_webhook.ex
@@ -8,8 +8,7 @@ defmodule KintaiViz.SlackWebhook do
 
   def handle_webhook(%{"type" => "message"} = params) do
     %{"channel" => channel, "text" => text, "ts" => ts, "user" => user} = params
-    # IO.puts "::::: Sleep 4000"
-    # Process.sleep(4000)
+
     IO.puts "::::: Save message '#{inspect params}'"
     Messages.create_slack_message(%{
       slack_user_id: user,
@@ -25,12 +24,13 @@ defmodule KintaiViz.SlackWebhook do
   end
 
   def create_user(slack_user_id) do
-    # Process.sleep(5000)
 
     case Accounts.get_user_by_slack_user_id(slack_user_id) do
       nil ->
         Accounts.create_user(%{slack_user_id: slack_user_id})
         IO.puts("Create user #{slack_user_id}")
+      slack_user ->
+        IO.puts("Found slack user #{slack_user.slack_user_id} ")
     end
   end
 
