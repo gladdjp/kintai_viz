36951dfd51800717927368c6ced9eea5d3183a36
Warut Surapat <s.warut@gmail.com>
2019-05-01 23:49:58 +0900

    Rename component.

--- assets/js/components/app.js
+++ assets/js/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Timeline from './timeline.js/index.js';
 
 class App extends Component {
   render() {
     return (
-      <div> App </div>
+      <div>
+        <div> Kintai Visualizer </div>
+        <Timeline />
+      </div>
     );
   }
 }
---
+++ assets/js/components/timeline.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        Channel Activity
+      </div>
+    );
+  }
+}
+
+export default Timeline;
