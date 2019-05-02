4b83435c5e04e960070870acd4f5b31652f89a07
Warut Surapat <s.warut@gmail.com>
2019-05-02 13:06:31 +0900

    Add timeline ui.

--- assets/js/components/timeline.js
+++ assets/js/components/timeline.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
 class Timeline extends Component {
+  renderSlots(label, items) {
+    return (
+      <div className='slot'>
+        <div className='slot-label'>
+          {label}
+        </div>
+        <div className='slot-items'>
+          {items.map((i) => (<div key={i}> {i} </div>))}
+        </div>
+      </div>
+    );
+  }
   render() {
     return (
-      <div>
-        Channel Activity
+      <div className='timeline'>
+        <div className='date'>
+          <div className='date-month'>
+            2 May
+          </div>
+          <div className='year'>
+            2019
+          </div>
+        </div>
+        <div className='slots'>
+          {this.renderSlots('13:00', ['a', 'b', 'c'])}
+          {this.renderSlots('13:30', ['a', 'b', 'c'])}
+          {this.renderSlots('14:00', ['a', 'b', 'c'])}
+          {this.renderSlots('14:30', ['a', 'b', 'c'])}
+        </div>
       </div>
     );
   }
