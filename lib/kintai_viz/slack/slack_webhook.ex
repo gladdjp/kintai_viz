85f73cd840d68fd577160a3999a0c55950fb99f1
Warut Surapat <wsurapat@gilt.jp>
2019-05-07 16:55:27 +0900

    Refactor slack webhook.

--- lib/kintai_viz/slack/slack_webhook.ex
+++ lib/kintai_viz/slack/slack_webhook.ex
@@ -7,25 +7,24 @@ defmodule KintaiViz.SlackWebhook do
   end
 
   def handle_webhook(%{"type" => "message"} = params) do
-    %{"channel" => channel, "text" => text, "ts" => ts, "user" => user} = params
-    kintai_time = get_kintai_time(text)
+    # Process.sleep(3000);
+    %{"channel" => channel, "text" => message, "ts" => ts, "user" => slack_user_id} = params
+    kintai_time = get_kintai_time(message)
     IO.puts "::::: Save message '#{inspect params}'"
     Messages.create_slack_message(%{
-      slack_user_id: user,
-      message: text,
       ts: ts,
       channel: channel,
-      client_msg_id:  "",
+      message: message,
+      slack_user_id: slack_user_id,
       kintai_time: kintai_time
     })
     KintaiVizWeb.Endpoint.broadcast(
       "slack_message:lobby",
       "message_created",
-      %{message: text, slack_user_id: user, kintai_time: kintai_time})
+      %{message: message, slack_user_id: slack_user_id, kintai_time: kintai_time})
   end
 
   def handle_webhook(_params) do
-    # Just do nothing
     IO.puts "::::: Unrecognize params, do nothing."
   end
 
@@ -33,7 +32,7 @@ defmodule KintaiViz.SlackWebhook do
     IO.puts "::::: Can't create/look-up user with nil value"
   end
   def create_user(slack_user_id) do
-    Process.sleep(500);
+    Process.sleep(5000);
     slack_user = case Accounts.get_user_by_slack_user_id(slack_user_id) do
       nil ->
         %{"user" => user} = Slack.Web.Users.info(slack_user_id)
@@ -61,7 +60,7 @@ defmodule KintaiViz.SlackWebhook do
   end
 
   defp get_kintai_time(message) do
-    pattern = ~r/(leave|本日|)(\d\d)(.*)(\d\d)(退勤|時退社|退社|に退社|ごろ退社|で退勤|退勤|時出社|出社)/u
+    pattern = ~r/(leave|本日)*(\d\d)(.*)(\d\d)(退勤|時退社|退社|に退社|ごろ退社|で退勤|退勤|時出社|出社)/u
     case Regex.run(pattern, message) do
       nil -> nil
       [_, _, hour, _, minute, _] -> "#{hour}:#{minute}"
